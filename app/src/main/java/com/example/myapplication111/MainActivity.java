e63f364fd49de77a69455e78d0b00c7ac813bf4f
yunsoo <04pys@naver.com>
2020-08-11 11:06:43 +0900

    jongsu cuteguy

--- app/src/main/java/com/example/myapplication111/MainActivity.java
+++ app/src/main/java/com/example/myapplication111/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication111;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //버튼 눌렸을떄
+               Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+               startActivity(intent);
+            }
+        });
+
     }
 }
